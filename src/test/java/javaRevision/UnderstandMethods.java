9162506ae7388aba715a14e6edd65f9dc0f30d88
arjun <arjun308569@gmail.com>
2025-02-06 15:26:52 +0530

    check that one

--- src/test/java/javaRevision/UnderstandMethods.java
+++ src/test/java/javaRevision/UnderstandMethods.java
@@ -1,8 +1,6 @@
 package javaRevision;
 
 public class UnderstandMethods {
-
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
@@ -11,21 +9,16 @@ public class UnderstandMethods {
 		names2();	
 		UnderstandMethods.names2();
 		
-		
-		
-		
-		
 	}
 	
 	
 	public void names()
 	{
 		System.out.println("Arjun Singh");
-<<<<<<< HEAD
+
 		System.out.println("How am i");
-=======
 		System.out.println("Arjun Singh how are you");
->>>>>>> 43ee1eee746330020bb37bc78798b16ec4dca471
+
 	}
 	
 	public static void names2()     //static methods can be called directly in the main method within the class.
