599b453e1b72070cb0bd9e4bf13e9889419e6cfb
arjun <arjun308569@gmail.com>
2025-02-06 15:31:33 +0530

    dependency

--- src/test/java/javaRevision/TestNGDataDrivenTesting.java
+++ src/test/java/javaRevision/TestNGDataDrivenTesting.java
@@ -2,6 +2,7 @@ package javaRevision;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
